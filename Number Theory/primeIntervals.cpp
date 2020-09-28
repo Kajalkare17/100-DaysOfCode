95cba7e043573408b6e616a70a7c86593a847712
Kajal Kare <kajalkare@ternaengg.ac.in>
2020-09-29 00:55:42 +0530

    primeIntervals

---
+++ Number Theory/primeIntervals.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isPrime(int n);
+
+bool isPrime(int n)
+{
+    if(n == 1)
+    return false;
+    for(int i = 2; i*i<=n; i++)
+        if(n%i == 0)
+        return false;
+    
+    return true;
+}
+int main()
+{
+    int l,r,i;
+    cin>>l>>r;
+    for(i = l; i<=r; i++)
+    if(isPrime(i))
+    cout<<i<<" ";
+}
